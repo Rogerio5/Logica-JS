b5fd850dc64a4acac703bfc4ef6fa0e8ba09635f
roger <rogeriosabino661gmail.com>
2025-07-28 22:04:30 -0300

    Adiciona página com desafios de JavaScript

---
+++ app.js
@@ -0,0 +1,25 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+let campo = document.querySelector(tag)
+campo.innerHTML = texto
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
